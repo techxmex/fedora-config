1140d3950d6355756c0c4d8b8f6f4e0789aeca87
techxmex <lvasquez6@gmail.com>
2022-06-16 16:45:27 -0500

    Update xfce_install.sh

--- xfce_install.sh
+++ xfce_install.sh
@@ -56,8 +56,8 @@ sudo dnf install -y celluloid
 sudo dnf install -y handbrake
 
 #install video codecs
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-sudo dnf install lame\* --exclude=lame-devel
+sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install -y lame\* --exclude=lame-devel
 sudo dnf group upgrade --with-optional Multimedia
 
 #install torrent app
